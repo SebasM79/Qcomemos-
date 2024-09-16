5a24d7eb346591c01731f64ac0578e22d58d572b
SebasM79 <sebamussi.dev@hotmail.com>
2024-09-15 22:15:30 -0300

    Actualizacion Login, index y conexion a DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,30 @@
 /**
  * Configuración de conexión a MongoDB
  */
+
+// js/db.js
+const { MongoClient } = require('mongodb');
+
+var database;
+
+async function connectToDatabase() {
+    const uri = 'mongodb+srv://almaypapa0304:almaypapa0304@qcomemos.bpclx.mongodb.net/?retryWrites=true&w=majority&appName=QComemos';  
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        console.log('Conexión exitosa a MongoDB');
+        database = client.db('QComemos');  //  nombre de la base de datos 
+        return database;
+    } catch (error) {
+        console.error('Error al conectarse a MongoDB:', error);
+        throw error;
+    }
+}
+
+function getDatabase() {
+    return database;
+}
+
+module.exports = { connectToDatabase, getDatabase };
+
