52f5efee4a5fef9ce0075fd07c12dcf240778246
SebasM79 <sebamussi.dev@hotmail.com>
2024-09-19 17:00:05 -0300

    modifificacion de direccion de usuario

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 // Ruta para registrar usuarios, que llama al controlador
-router.post('/api/usuarios', authController.registrarUsuario);
+router.post('/usuarios', authController.registrarUsuario);
 
 module.exports = router;
